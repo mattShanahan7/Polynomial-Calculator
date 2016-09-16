7260f7ae3b4137c917f54c9936e4cadca41f396c
Matthew <Matthew@matts-macbook-pro-6.local>
2016-09-16 16:46:11 -0400

    Initial Commit

---
+++ Assignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by Matthew on 9/16/16.
+//  Copyright © 2016 Matthew. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
